3978db4db4d1d94e323bb6733c1e2811cab3a593
luuk <lah.vanberkel@student.avans.nl>
2023-04-06 09:09:04 +0200

    Fixed it

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,3 @@
-// #include <Arduino.h>
-// #include <ld08.hpp>
-// #include <ros.h>
-// #include <std_msgs/String.h>
-// #include <sensor_msgs/LaserScan.h>
-
-// ros::NodeHandle node_handler;
-
-// sensor_msgs::LaserScan lidar_msg;
-// ros::Publisher laser_pub("laser_scan_publisher", &lidar_msg);
-
-// void setup() {
-//   // Starting rosserial.
-//   node_handler.initNode();
-//   node_handler.advertise(laser_pub);
-
-//   // Starting lidar.
-//   lidar.begin();
-
-//   //  Buffers that are used for reading.
-// }
-
-// void loop() {
-//   if (lidar.read_frame(&frame)){
-//     lidar_msg.header.frame_id = "base_scan";
-//     uint32_t diff = ((uint32_t)frame.end_angle + 36000 - (uint32_t)frame.start_angle) % 36000;
-//     lidar_msg.angle_increment = diff / (POINT_PER_PACK - 1) / 100.0;
-//     lidar_msg.angle_min = static_cast<double>(frame.start_angle) / 100.0;
-//     lidar_msg.angle_max = static_cast<double>(frame.end_angle % 36000) / 100.0;
-//     lidar_msg.time_increment = (360 / frame.speed) / (( lidar_msg.angle_max -   lidar_msg.angle_min) /  lidar_msg.angle_increment);
-//     lidar_msg.range_max = 0; 
-//     lidar_msg.range_min = 0;
-//     lidar_msg.scan_time = 360 / frame.speed;
-//     for (size_t i = 0; i < POINT_PER_PACK; i++)
-//     {
-//       if (lidar_msg.range_max < (frame.point[i].distance / 1000.f)){
-//         lidar_msg.range_max = frame.point[i].distance / 1000.f;
-//         lidar_msg.intensities[0] = frame.point[i].confidence;
-//       }
-//       else if (lidar_msg.range_min > (frame.point[i].distance / 1000.f)){
-//         lidar_msg.angle_min = frame.point[i].distance / 1000.f;
-//       }
-//       lidar_msg.ranges[i] = frame.point[i].distance / 1000.f;
-//       lidar_msg.intensities[i] = frame.point[i].confidence;
-      
-//       laser_pub.publish(&lidar_msg);
-//       node_handler.spinOnce();
-//     }
-//   }
-//   delay(1000);
-// }
-
 #include <ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <Arduino.h>
@@ -102,7 +50,6 @@ void loop()
       output.range_max = range_max;
       output.range_min = range_min;
       output.angle_increment = angle_increment;
-      //output.time_increment =  (360.0f / frame.speed) / ((frame.speed - angle_min) / angle_increment);
       output.scan_time = 360.0f / frame.speed;
     
       // Pushing pack
